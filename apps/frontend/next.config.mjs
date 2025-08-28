8276f5fe89fae676c307a184725cf9b55b69023e
alendra1945 <dwialendraipangselly23@gmail.com>
2025-08-29 02:36:14 +0700

    update environment

--- apps/frontend/next.config.mjs
+++ apps/frontend/next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: `${'http://localhost:3001'}/api/:path*`, // send it to your API
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`,
       },
     ];
   },
